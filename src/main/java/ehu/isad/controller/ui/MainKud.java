0464507fd96ee1bc045c0fd5e7da6387361a2bd9
Aitor <aelorza034@ehu.eus>
2020-11-17 20:53:42 +0100

    Orokorrean aplikazioaren interfazearen elementuak jarri dira, estetika landu da baina metodoek eta botoiak ez dute ezer egiten.
    Main.fxml / WhatWeb.fxml / Zerbitzariak.fxml
    Interfazearen elementu guztien implementazioa baina ActionEvent falta da egitko.
    
    MainKud.java
    Button-etan click egiteko metodoak implementatu egin dira
    
    whatWebKud.java
    WhatWeb kudeatzailearen interfazearen implementazioa.
    
    zerbitzariakKud.java
    Kontsultatutako zerbitzarien zerrendaketa interfazearen kudeatzailearen eskeletoa implementatu da.

--- src/main/java/ehu/isad/controller/ui/MainKud.java
+++ src/main/java/ehu/isad/controller/ui/MainKud.java
@@ -6,10 +6,12 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -22,7 +24,21 @@ import java.util.stream.Collectors;
 public class MainKud implements Initializable {
 
   private Main mainApp;
+  @FXML
+  private AnchorPane cms;
+  @FXML
+  private Button cms_button;
 
+  @FXML
+  private Button zerbitzari_button;
+
+  @FXML
+  private Button whatWeb_button;
+  @FXML
+  private AnchorPane zerbitzariak;
+
+  @FXML
+  private AnchorPane WhatWeb;
   @FXML
   private FontAwesomeIconView btnClose;
 
@@ -44,7 +60,6 @@ public class MainKud implements Initializable {
 
 
     Thread taskThread = new Thread(() -> {
-
       String newLine = System.getProperty("line.separator");
       final StringBuilder emaitza = new StringBuilder();
       this.allProcesses().forEach(line -> {
@@ -64,8 +79,27 @@ public class MainKud implements Initializable {
 
   }
   @FXML
+  void leihoa_aldatu(ActionEvent event) {
+    if (event.getSource().equals(whatWeb_button)) {
+      whatWeb_button.toFront();
+      zerbitzariak.toBack();
+      cms.toBack();
+    }
+    if (event.getSource().equals(zerbitzari_button)) {
+      zerbitzariak.toFront();
+      cms.toBack();
+      WhatWeb.toBack();
+    }
+    if (event.getSource().equals(cms_button)) {
+      cms.toFront();
+      WhatWeb.toBack();
+      zerbitzariak.toBack();
+    }
+  }
+  @FXML
   void handleClose(MouseEvent event) {
     System.exit(0);
+
   }
   @Override
   public void initialize(URL location, ResourceBundle resources) {
---
+++ src/main/java/ehu/isad/controller/ui/whatWebKud.java
@@ -0,0 +1,37 @@
+package ehu.isad.controller.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class whatWebKud implements Initializable {
+
+    @FXML
+    private TextField url_txt_1;
+
+    @FXML
+    private TextArea log_txt_1;
+
+    @FXML
+    private Button eskaneatu_button_1;
+
+    @FXML
+    private TextField url_txt_2;
+
+    @FXML
+    private TextArea log_txt_2;
+
+    @FXML
+    private Button eskaneatu_button_2;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
