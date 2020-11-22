3bafaeaafe68ed59fa897039cdb8cb5acb200e32
ortiwii <ergorka@gmail.com>
2020-11-22 17:50:15 +0100

    Datu basera txertaketa kudeatu

--- src/main/java/ehu/isad/Services/WhatWebConection.java
+++ src/main/java/ehu/isad/Services/WhatWebConection.java
@@ -1,13 +1,16 @@
 package ehu.isad.Services;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 public class WhatWebConection {
 
-    private static WhatWebConection instantzia = null;
+    private static WhatWebConection instantzia = new WhatWebConection();
 
     private WhatWebConection (){ }
 
@@ -16,16 +19,29 @@ public class WhatWebConection {
     }
     public List<String> allProcesses(String url) {
         List<String> processes = new LinkedList<String>();
-        if (!url.equals("")) {
+        if (!url.equals("") ) {
             try {
+                Properties properties = null;
+                InputStream in = null;
+
+                in = this.getClass().getResourceAsStream("/setup.properties");
+                properties = new Properties();
+                properties.load(in);
+                String root = System.getProperty("user.dir");
+                String tmp = properties.getProperty("pathToInsert");
+                String path = root+tmp;
+
                 String line;
                 Process p = null;
                 if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                    System.out.println(System.getProperty("user"));
+                    path = path.replace("\\","/" );
+                    path = path.replace("D:", "/mnt/d");
+                    path = path.replace(" ", "\\ ");
                     p = Runtime.getRuntime().exec
-                            (System.getenv("windir") + "\\system32\\" + "wsl whatweb --colour=never --log-sql=/tmp/insert.sql "+url);
+                            (System.getenv("windir") + "\\system32\\" + "wsl whatweb --colour=never --log-sql="+path+" "+url);
                 } else {
-                    p = Runtime.getRuntime().exec("whatweb --colour=never --log-sql=/tmp/insert.sql"+url);
+                    path = path.replace(" ", "\\ ");
+                    p = Runtime.getRuntime().exec("whatweb --colour=never --log-sql="+path+" "+url);
                 }
                 BufferedReader input =
                         new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -39,4 +55,6 @@ public class WhatWebConection {
         }
         return processes;
     }
+
+
 }
--- src/main/java/ehu/isad/controller/ui/CMSKud.java
+++ src/main/java/ehu/isad/controller/ui/CMSKud.java
@@ -5,11 +5,14 @@ import java.util.ResourceBundle;
 
 import ehu.isad.Services.WhatWebConection;
 import ehu.isad.controller.db.DBKudeatzaile;
+import ehu.isad.controller.db.WhatWebDBKud;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class CMSKud {
@@ -28,34 +31,39 @@ public class CMSKud {
 
     @FXML
     void onClick(ActionEvent event) {
+
+        this.bilatu();
+
+    }
+    @FXML
+    void keyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER){
+            this.bilatu();
+        }
+    }
+    @FXML
+    void initialize() {
+
+    }
+    private void bilatu (){
         txtArea.setWrapText(true);
         txtArea.setText("Kargatzen. Itxaron, mesedez....");
 
-
-        Thread taskThread = new Thread(() -> {
+        Thread thread = new Thread(() -> {
+            //TODO --> SOILIK BILAKETA EGIN BEHAR DA DATU BASEAN EZ DAGOENEAN, BERAZ LEHENIK ETA BEHIN bilatutaDago() ??
             String newLine = System.getProperty("line.separator");
             final StringBuilder emaitza = new StringBuilder();
-            WhatWebConection.getInstance().allProcesses(urlArea.getText()).forEach(line -> {
+            String url = urlArea.getText();
+            WhatWebConection.getInstance().allProcesses(url).forEach(line -> {
                 emaitza.append(line + newLine);
             });
 
             Platform.runLater(() -> {
-                System.out.println("Aurkituta");
                 txtArea.setText(emaitza.toString());
-
-
-
-
+                WhatWebDBKud.getInstance().txertatu(url);
             });
-
         });
 
-        taskThread.start();
-
-    }
-
-    @FXML
-    void initialize() {
-
+        thread.start();
     }
 }
