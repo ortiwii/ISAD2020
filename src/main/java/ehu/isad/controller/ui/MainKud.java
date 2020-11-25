6df91d8a6076c55e3b7053ce4fa6d1a255dd9011
Aitor <aelorza034@ehu.eus>
2020-11-25 12:14:48 +0100

    Interfazearen estilo aldaketa batzuk.

--- src/main/java/ehu/isad/controller/ui/MainKud.java
+++ src/main/java/ehu/isad/controller/ui/MainKud.java
@@ -4,14 +4,18 @@ import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import ehu.isad.Main;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.ColorInput;
+import javafx.scene.effect.Effect;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -30,6 +34,15 @@ public class MainKud implements Initializable {
   private Button cms_button;
 
   @FXML
+  private Pane izen_panela;
+
+  @FXML
+  private Label azpititulua;
+
+  @FXML
+  private Label titulua;
+
+  @FXML
   private Button zerbitzari_button;
 
   @FXML
@@ -46,28 +59,42 @@ public class MainKud implements Initializable {
     this.mainApp = mainApp;
   }
 
-//  @FXML
-//  public void onClick(ActionEvent actionEvent) {
-//
-//
-//  }
   @FXML
   void leihoa_aldatu(ActionEvent event) {
     if (event.getSource().equals(whatWeb_button)) {
+      cms_button.setEffect(new Blend());
+      izen_panela.setBackground(new Background(new BackgroundFill(Color.rgb(252,238,37),CornerRadii.EMPTY,Insets.EMPTY)));
+      titulua.setText("WHATWEB");
+      azpititulua.setText("Web orrialdeak eskanerra");
       whatWeb_button.toFront();
       zerbitzariak.toBack();
       cms.toBack();
     }
     if (event.getSource().equals(zerbitzari_button)) {
+      izen_panela.setBackground(new Background(new BackgroundFill(Color.rgb(156,156,156), CornerRadii.EMPTY, Insets.EMPTY)));
+      cms_button.setEffect(new Blend());
+      titulua.setText("ZERBITZARIEN ZERRENDA");
+      azpititulua.setText("Kontsultatu diren zerbitzarien zerrenda");
       zerbitzariak.toFront();
       cms.toBack();
       WhatWeb.toBack();
     }
     if (event.getSource().equals(cms_button)) {
+      izen_panela.setBackground(new Background(new BackgroundFill(Color.MEDIUMPURPLE, CornerRadii.EMPTY, Insets.EMPTY)));
+      cms_button.setEffect(new Blend());
+      titulua.setText("CMS");
+      azpititulua.setText("WordPress, Joomla, PhpMyAdmin, Drupal");
       cms.toFront();
       WhatWeb.toBack();
       zerbitzariak.toBack();
     }
+  }
+  public void hasieraketak_egin(){
+    titulua.setText("CMS");
+    azpititulua.setText("WordPress, Joomla, PhpMyAdmin, Drupal");
+    izen_panela.setBackground(new Background(new BackgroundFill(Color.MEDIUMPURPLE, CornerRadii.EMPTY, Insets.EMPTY)));
+    cms_button.setEffect(new Blend(BlendMode.ADD));
+
   }
   @FXML
   void handleClose(MouseEvent event) {
